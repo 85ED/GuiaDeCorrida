62699083b5ca7fca25daa8122bec5a9c84864f12
85ED <edsonfelixnet@gmail.com>
2025-06-16 01:11:18 -0300

    ajuste de porta

--- public/js/login.js
+++ public/js/login.js
@@ -166,7 +166,7 @@ function fazerLogin() {
     return;
   }
 
-  fetch("http://localhost:3000/usuarios/autenticar", {
+  fetch("https://guiadecorrida.onrender.com/usuarios/autenticar", {
     method: "POST",
     headers: { "Content-Type": "application/json" },
     body: JSON.stringify(dados),
